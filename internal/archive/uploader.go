5284f8331d31eceddee35887ff042ad65636470f
iwanhae <iwanhae@gmail.com>
2025-09-22 14:35:07 +0000

    덕지덕지

--- internal/archive/uploader.go
+++ internal/archive/uploader.go
@@ -3,6 +3,7 @@ package archive
 import (
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -34,8 +35,51 @@ func UploadZip(filename string, file io.Reader, maxSize int64) error {
 	// Move to data dir
 	dataDir := config.Get().Data.Dir
 	targetPath := filepath.Join(dataDir, filepath.Base(filename))
-	if err := os.Rename(tempFile.Name(), targetPath); err != nil {
+
+	if err := rename(tempFile.Name(), targetPath); err != nil {
 		return fmt.Errorf("failed to save file: %w", err)
+	}
+
+	return nil
+}
+
+func rename(srcPath, dstPath string) error {
+	if err := os.Rename(srcPath, dstPath); err != nil {
+		slog.Warn("failed to rename", "src", srcPath, "dst", dstPath, "err", err.Error())
+	} else if err == nil {
+		return nil
+	}
+
+	// Open the source file
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %w", err)
+	}
+	defer srcFile.Close()
+
+	// Create the destination file
+	dstFile, err := os.Create(dstPath)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %w", err)
+	}
+	defer dstFile.Close()
+
+	// Copy content from source to destination
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return fmt.Errorf("failed to copy file content: %w", err)
+	}
+
+	// Ensure all data is written to disk
+	err = dstFile.Sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync destination file: %w", err)
+	}
+
+	// Remove the source file
+	err = os.Remove(srcPath)
+	if err != nil {
+		return fmt.Errorf("failed to remove source file: %w", err)
 	}
 
 	return nil
